f34ea72b85ff710acb17c0984c11fda2bbaab55c
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2023-04-03 11:57:46 +0200

    Added some basic routes

--- 03.04.2023_routing/server.js
+++ 03.04.2023_routing/server.js
@@ -2,6 +2,24 @@ import express from "express";
 
 const app = express();
 
+// this line of code will handle GET requests to /
+app.get("/", (request, response) => {
+  console.log("Request received");
+
+  response.send("Thankyou for your request");
+});
+
+app.get("/account", (request, response) => {
+  response.send("Your account details are PIN: 23492843294234");
+});
+
+// here username is a route parameters
+app.post("/login/:username", (request, response) => {
+  console.log(request.params.username);
+
+  response.send(`Your username is: ${request.params.username}`);
+});
+
 app.listen(3002, () => {
   console.log("The server is listening...🐒");
 });
