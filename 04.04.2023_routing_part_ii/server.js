4cec8fb0577e504246bf844a54c2c082e36d17ad
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2023-04-04 11:25:10 +0200

    Added endpoints

--- 04.04.2023_routing_part_ii/server.js
+++ 04.04.2023_routing_part_ii/server.js
@@ -3,6 +3,28 @@ import express from "express";
 const app = express();
 const port = 4000;
 
+app.get("/api/symbols", (request, response) => {
+  response.send("Symbols endpoint");
+});
+
+app.get("/api/latest", (request, response) => {
+  response.send("Latest endpoint");
+});
+
+app.get("/api/convert", (request, response) => {
+  response.send("Convert endpoint");
+});
+
+app.get("/api/:date", (request, response) => {
+  const timestamp = Date.parse(request.params.date);
+
+  if (isNaN(timestamp)) {
+    response.send("This is not a valid date");
+  }
+
+  response.send("This is a valid date! 👍");
+});
+
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}...🐒`);
 });
