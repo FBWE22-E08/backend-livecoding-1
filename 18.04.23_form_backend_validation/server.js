c28aa477168c52dfff85ad10ae7fd6e852bef4bd
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2023-04-18 10:13:28 +0200

    Intial commit 2

---
+++ 18.04.23_form_backend_validation/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import apiRouter from "./routes/api.js";
+
+const app = express();
+const port = 4000;
+
+// middleware
+app.use(express.json());
+app.use("/api", apiRouter);
+
+app.listen(port, () => {
+  console.log(`The server is listening on port ${port}...🐒`);
+});
